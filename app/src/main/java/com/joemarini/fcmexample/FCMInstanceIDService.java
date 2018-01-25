4a1e386d92b1d2d23086787a9c5a16e1f1dc302d
inec <luyege+github@gmail.com>
2018-01-25 00:24:55 -0600

    'idserverjsav'

---
+++ app/src/main/java/com/joemarini/fcmexample/FCMInstanceIDService.java
@@ -0,0 +1,8 @@
+package com.joemarini.fcmexample;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FCMInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "FCMInstanceIDService";
+
+}
