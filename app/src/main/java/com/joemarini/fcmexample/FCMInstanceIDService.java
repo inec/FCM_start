5fdb3d05aa56fadb188b0f090095b02e3b44d81a
inec <luyege+github@gmail.com>
2018-01-25 23:45:35 -0600

    'test'

--- app/src/main/java/com/joemarini/fcmexample/FCMInstanceIDService.java
+++ app/src/main/java/com/joemarini/fcmexample/FCMInstanceIDService.java
@@ -2,7 +2,7 @@ package com.joemarini.fcmexample;
 
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class FCMInstanceIDService extends FirebaseInstanceIdService {
+public class FCMInstanceIDService extends fr  {
     private static final String TAG = "FCMInstanceIDService";
 
 }
